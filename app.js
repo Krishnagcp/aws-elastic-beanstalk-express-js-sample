1eeac73bb8d89c7f4e5a83102377cd8d7a51875f
Krishnagcp <krishnaggkbt@gmail.com>
2024-04-07 13:05:02 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Have a great day'));
+app.get('/', (req, res) => res.send('Hello friends'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
